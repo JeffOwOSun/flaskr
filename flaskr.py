8fb60bff60c50aeb6c870ec3cbf4bb95d81e1a2a
JeffOwOSun <sunyushi9546@gmail.com>
2014-06-20 02:37:19 -0700

    made database connections

--- flaskr.py
+++ flaskr.py
@@ -23,6 +23,20 @@ def connect_db():
     rv.row_factory=sqlite3.Row
     return rv
 
+def get_db():
+    """Opens a new database connection if there is none yet for the current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request. This is the reaction function of the event teardown appcontext. error here is the error code passed in when the teardown function is called"""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
 if __name__=='__main__':
     app.run()
 
