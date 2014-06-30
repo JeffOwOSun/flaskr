9a5bbaf2c62e4ad06c5bafe24a4dced7c348b69b
JeffOwOSun <sunyushi9546@gmail.com>
2014-06-30 14:38:57 +0800

    add entry was not complete. Ohmy. Sorry

--- flaskr.py
+++ flaskr.py
@@ -55,8 +55,12 @@ def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
-    db.execute('insert into entries (title, text) values (?,?)', [request.form['title'],request.form['text']])
-
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+    
 @app.route('/login', methods=['GET','POST'])
 def login():
     error=None
